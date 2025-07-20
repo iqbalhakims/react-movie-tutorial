5331b99463d27fe08a1801ca527387a6344fa9b0
Iqbal Fauzi <iqbalfauzi@Iqbals-MacBook-Air.local>
2025-07-20 21:34:19 +0800

    create thumbnail

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import MovieCard from './components/MovieCard.jsx'
+
+function App() {
+<>
+  <MovieCard movie={{title: "IQBALs FIlM", release_date: "2024" }}/>
+  <MovieCard movie={{title: " Jeos Film", release_date: "2025" }}/>
+  </>
+}
+
+export default App
