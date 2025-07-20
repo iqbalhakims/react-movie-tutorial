e67b644014853c6da7ac663a73c0937d46caa09c
Iqbal Fauzi <iqbalfauzi@Iqbals-MacBook-Air.local>
2025-07-20 23:17:48 +0800

    add return app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import MovieCard from './components/MovieCard.jsx'
 
 function App() {
+return(
 <>
   <MovieCard movie={{title: "IQBALs FIlM", release_date: "2024" }}/>
   <MovieCard movie={{title: " Jeos Film", release_date: "2025" }}/>
   </>
+)
 }
 
 export default App
